1b6df22453f81bc223244df721bcd89a2fdd80f3
Anonymous <Anonymous@172.24.176.5>
2013-10-17 16:17:48 -0400

    - Began work on a Settings tab in the level editor.

---
+++ src/Core/LevelEditor/Panels/Tabs/SettingsTab.java
@@ -0,0 +1,46 @@
+package Core.LevelEditor.Panels.Tabs;
+
+import Level.Level;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class SettingsTab extends JPanel implements ActionListener {
+   private Level mLevel;
+   
+   public SettingsTab(Level level) {
+      mLevel = level;
+      createWidgets();
+   }
+   
+   private void createWidgets() {
+      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+      createNameField();
+      add(Box.createVerticalStrut(5));
+      createBackgroundField();
+      add(Box.createVerticalGlue());
+   }
+   
+   private void createNameField() {
+      JPanel namePanel = new JPanel();
+      namePanel.add(new JLabel("Name:"));
+      JTextField nameField = new JTextField(10);
+      nameField.setText(mLevel.name);
+      namePanel.add(nameField);
+      add(namePanel);
+   }
+   
+   private void createBackgroundField() {
+      
+   }
+   
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      
+   }
+
+}
