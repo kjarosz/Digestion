e5857d0e35ca3b4421ff9d0e4638b347ed20ca72
boogyman19946 <boogyman19946@gmail.com>
2014-02-05 13:54:15 -0500

    Create window on EDT.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 
 
+import javax.swing.SwingUtilities;
+
 import Core.Game.Game;
 import Util.ErrorLog;
 
 public class Main {
 	public static void main(String arguments[]) {
-      try {
-         new Game();
-      } catch(Exception e) {
-         ErrorLog errorLog = ErrorLog.getInstance();
-         errorLog.displayMessageDialog(e.getMessage());
-      }
+	   SwingUtilities.invokeLater(new Runnable() {
+	      @Override
+	      public void run() {
+	         try {
+	            new Game();
+	         } catch(Exception e) {
+	            ErrorLog errorLog = ErrorLog.getInstance();
+	            errorLog.displayMessageDialog(e.getMessage());
+	         }
+	         
+	      }
+	   });
 	}
 }
