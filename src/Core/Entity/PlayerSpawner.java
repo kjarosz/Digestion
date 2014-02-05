16b0d3b60464ae12ac40e3b08a2462c350743589
boogyman19946 <boogyman19946@gmail.com>
2014-02-05 13:59:44 -0500

    Change vector constants.

--- src/Core/Entity/PlayerSpawner.java
+++ src/Core/Entity/PlayerSpawner.java
@@ -23,8 +23,8 @@ public class PlayerSpawner extends EntitySpawner {
    private final Vector2D ZERO_VECTOR = new Vector2D(0, 0);
    private final Vector2D LEFT_ACCELERATION = new Vector2D(-250, 0);
    private final Vector2D RIGHT_ACCELERATION = new Vector2D(250, 0);
-   private final Vector2D JUMP_VELOCITY = new Vector2D(0, -200);
-   private final Vector2D DOWN_ACCELERATION = new Vector2D(0, 120000); 
+   private final Vector2D JUMP_VELOCITY = new Vector2D(0, -700);
+   private final Vector2D DOWN_ACCELERATION = new Vector2D(0, 120); 
    
    @Override
    public int spawn(EntityComponents components) {
@@ -50,7 +50,7 @@ public class PlayerSpawner extends EntitySpawner {
       movable.acceleration.y = Movable.GRAVITY;
       movable.velocity.x = 0.0;
       movable.velocity.y = 0.0;
-      movable.maximumSpeed = 200.0;
+      movable.maximumSpeed = 400.0;
       movable.lastTime = System.currentTimeMillis();
       return World.ENTITY_MOVABLE;
    }
@@ -62,8 +62,8 @@ public class PlayerSpawner extends EntitySpawner {
    }
    
    private int makeControllable(Controllable controllable) {
-      constructMovingKeyMapping(controllable, KeyEvent.VK_A, LEFT_ACCELERATION, ZERO_VECTOR);      
-      constructMovingKeyMapping(controllable, KeyEvent.VK_W, ZERO_VECTOR, JUMP_VELOCITY);
+      constructMovingKeyMapping(controllable, KeyEvent.VK_A, LEFT_ACCELERATION, ZERO_VECTOR);
+      constructJumpKeyMapping(controllable, KeyEvent.VK_W, ZERO_VECTOR, JUMP_VELOCITY);
       constructMovingKeyMapping(controllable, KeyEvent.VK_D, RIGHT_ACCELERATION, ZERO_VECTOR);
       constructMovingKeyMapping(controllable, KeyEvent.VK_S, DOWN_ACCELERATION, ZERO_VECTOR);
       
@@ -83,6 +83,37 @@ public class PlayerSpawner extends EntitySpawner {
          
          @Override
          public void keyReleased(EntityComponents components) {
+            components.movable.acceleration.subtract(changeAcceleration);
+         }
+      };
+      keyMapping.pressProcessed = false;
+      keyMapping.releaseProcessed = true;
+      
+      controllable.keyMappings.add(keyMapping);
+   }
+   
+   private void constructJumpKeyMapping(Controllable controllable, int keyCode,
+         final Vector2D changeAcceleration, final Vector2D changeVelocity) {
+      KeyMapping keyMapping = new KeyMapping();
+      keyMapping.keyCode = keyCode;
+      keyMapping.keyFunction = new ControlFunction() {
+         @Override
+         public void keyPressed(EntityComponents components) {
+            if(components.movable.jumped) {
+               if(components.movable.doubleJumped) {
+                  return;
+               } else {
+                  components.movable.doubleJumped = true;
+               }
+            } else {
+               components.movable.jumped = true;
+            }
+            
+            components.movable.acceleration.add(changeAcceleration);
+            components.movable.velocity.add(changeVelocity);
+         }
+         @Override
+         public void keyReleased(EntityComponents components) { 
             components.movable.acceleration.subtract(changeAcceleration);
          }
       };
