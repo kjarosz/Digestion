9d4081a1b3d6b3c754317e9d9763625e01222c18
boogyman19946 <boogyman19946@gmail.com>
2014-02-07 16:55:27 -0500

    Changed gravity from a scalar to vector.

--- src/Level/Level.java
+++ src/Level/Level.java
@@ -1,18 +1,21 @@
 package Level;
 
-import Util.Size;
 import java.awt.image.BufferedImage;
+
+import org.jbox2d.common.Vec2;
+
+import Util.Size;
 
 public class Level {
    public String name;
 	public Size size;
-	public double gravity;
+	public Vec2 gravity;
    
    public BufferedImage mBackground;
 
 	public Level() {
       name = "";
       size = new Size(800, 600);
-      gravity = 9.8;
+      gravity = new Vec2(0.0f, 9.81f);
 	}
 }
