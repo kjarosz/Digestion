2038e23e9c9940a687e0581cfeba89a7190fd977
boogyman19946 <boogyman19946@gmail.com>
2014-02-11 14:54:56 -0500

    Added methods to invertYAxis and unit conversion to CanvasInterface.

--- src/Graphics/CanvasInterface.java
+++ src/Graphics/CanvasInterface.java
@@ -3,8 +3,17 @@ package Graphics;
 import java.awt.Image;
 
 public interface CanvasInterface {
+   public void       invertYAxis(boolean flag);
+   
+   /*
+    * @param factor: Pixels per unit. ( ex. 32 pixels per 1 meter: 32.0f / 1.0f = 32.0f
+    * 
+    * To reset conversion factor, call the function with 1.0f as argument.
+    */
+   public void       setUnitConversionFactor(float factor);
+   
    public ColorMode  setGraphicsMode(ColorMode newMode);
    public void       drawImage(ImageItem imageItem);
-   public void       drawImage(Image image, int x, int y, int z, int width, int height);
+   public void       drawImage(Image image, float x, float y, float z, float width, float height);
    public void       showCanvas();
 }
