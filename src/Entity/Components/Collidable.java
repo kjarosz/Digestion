0b96c7d9184b2c31d7d9bd2e0851e5afbee898de
boogyman19946 <boogyman19946@gmail.com>
2014-02-10 20:51:35 -0500

    Changed Movable and Collidable classes to fit Box2D's model.

--- src/Entity/Components/Collidable.java
+++
@@ -1,7 +0,0 @@
-package Entity.Components;
-
-public class Collidable {
-   public boolean bindToImageDimensions;
-   public double width;
-   public double height;
-}
--- src/Entity/Components/Movable.java
+++ src/Entity/Components/Movable.java
@@ -1,29 +1,12 @@
 package Entity.Components;
 
-import Util.Vector2D;
+import org.jbox2d.common.Vec2;
 
 public class Movable {
-   public final static double GRAVITY = 500;
-   
-   public boolean jumped;
-   public boolean doubleJumped;
-   
-   public double maximumSpeed;
-   public Vector2D velocity;
-   public Vector2D acceleration;
-   public double mass;
-   public double frictionCoefficient;
-   public long lastTime;
+	
+	public Vec2 actingForces;
    
    public Movable() {
-      jumped = false;
-      doubleJumped = false;
-      
-      maximumSpeed = 0.0;
-      velocity = new Vector2D(0.0, 0.0);
-      acceleration = new Vector2D(0.0, 0.0);
-      mass = 0.0;
-      frictionCoefficient = 0.0;
-      lastTime = System.currentTimeMillis();
+   	actingForces = new Vec2(0.0f, 0.0f);
    }
 }
