3b3587d04634aab7d082a55ba55dd56973d059a7
boogyman19946 <boogyman19946@gmail.com>
2014-09-26 15:17:57 -0400

    Corrected timing in MotionSystem.

--- src/Entity/Systems/MotionSystem.java
+++ src/Entity/Systems/MotionSystem.java
@@ -5,26 +5,24 @@ import org.jbox2d.dynamics.World;
 import Level.EntityContainer;
 
 public class MotionSystem {
-   private static final float TIME_STEP = 1.0f / 60.0f;
-   private static float sLastTime = 0;
+	private static long sLastTime = 0;
    
    public static void move(World box2DWorld, EntityContainer world) {
       
-      float timeStep;
-      while((timeStep = getTimeStep()) > 0.0f)
-         box2DWorld.step(timeStep, 6, 2);
+      box2DWorld.step((float)nanoToSeconds(getElapsedTime()), 6, 2);
    }
    
-   private static float getTimeStep() {
-      if((System.currentTimeMillis() - sLastTime) > TIME_STEP) {
-         sLastTime += TIME_STEP;
-         return sLastTime;
-      } else {
-         return 0.0f;
-      }
+   private static long getElapsedTime() {
+   	long elapsedTime = System.nanoTime() - sLastTime;
+   	sLastTime += elapsedTime;
+   	return elapsedTime;
    }
    
    public static void resetTimer() {
-      sLastTime = System.currentTimeMillis();
+   	sLastTime = System.nanoTime();
+   }
+   
+   private static double nanoToSeconds(double nano) {
+   	return nano / 1000000000.0;
    }
 }
