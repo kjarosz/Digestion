199c63df5c694c6f2756a0a0d73f49f052c6dc05
Kamil Jarosz <kjarosz19946@gmail.com>
2015-04-14 17:16:00 -0400

    Changed extension to JSON and spanwed level model writer.

--- src/Core/LevelEditor/Components/EditorToolbar.java
+++ src/Core/LevelEditor/Components/EditorToolbar.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.util.concurrent.ExecutionException;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -14,6 +15,7 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import Core.LevelEditor.Models.LevelModel;
+import Core.LevelEditor.Utils.LevelModelWriter;
 import Menu.MenuStack;
 
 public class EditorToolbar extends JToolBar {
@@ -84,9 +86,7 @@ public class EditorToolbar extends JToolBar {
       return new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
-            if(mUnsaved) {
-               //save();
-            }
+            save();
          }
       };
    }
@@ -95,7 +95,7 @@ public class EditorToolbar extends JToolBar {
       return new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
-            //saveAs();
+            saveAs();
          }
       };
    }
@@ -114,7 +114,44 @@ public class EditorToolbar extends JToolBar {
    
    /* ********************************************************************** */
    /*                               UTILITIES                                */
-   /* ********************************************************************** */   
+   /* ********************************************************************** */
+   private void save() {
+      if(mLevelFile == null) {
+         saveAs();
+      } else {
+         LevelModelWriter writer = new LevelModelWriter(mLevelFile, mLevelModel);
+         writer.addPropertyChangeListener(createWriterListener());
+         writer.execute();
+      }
+   }
+   
+   private PropertyChangeListener createWriterListener() {
+      return new PropertyChangeListener() {
+         @Override
+         public void propertyChange(PropertyChangeEvent e) {
+            String prop = e.getPropertyName();
+            if(!"state".equals(prop)) {
+               return;
+            }
+            
+            String state = (String)e.getNewValue();
+            if("done".equals(state)) {
+               LevelModelWriter writer = (LevelModelWriter)e.getSource();
+               try {
+                  mLevelFile = writer.get();
+               } catch (InterruptedException | ExecutionException e1) {
+                  e1.printStackTrace();
+               }
+            }
+         }
+      };
+   }
+   
+   private void saveAs() {
+      mLevelFile = chooseNewLevelFile();
+      save();
+   }
+   
    private boolean dealWithUnsaved() {      
       return !mUnsaved || unsavedDiscardAction();
    }
@@ -126,7 +163,7 @@ public class EditorToolbar extends JToolBar {
             "Unsaved Changes",
             JOptionPane.YES_NO_CANCEL_OPTION);
       if(choice == JOptionPane.YES_OPTION) {
-         //save();
+         save();
          return true;
       } else if(choice == JOptionPane.NO_OPTION) {
          return true;
@@ -137,7 +174,7 @@ public class EditorToolbar extends JToolBar {
    
    private FileFilter getLevelFileFilter() {
       return new FileNameExtensionFilter(
-            "Level Script", ".py");
+            "Level Script", ".json");
    }
    
    private JFileChooser getLevelFileChooser() {
@@ -171,14 +208,10 @@ public class EditorToolbar extends JToolBar {
    
    private File validateFilename(File file) {
       String name = file.getName();
-      if(name.endsWith(".py")) {
+      if(name.endsWith(".json")) {
          return file;
       }
-      return new File(file.getPath(),
-            name.concat(".py"));
+      return new File(file.getParent(),
+            name.concat(".json"));
    }
-   
-     //////////////////////////////////////////////////////////////////////////
-    ////////////////// OLD BULLSHIT //////////////////////////////////////////
-   //////////////////////////////////////////////////////////////////////////
 }
