c897ee9245670e46d10a7a7e59ea4410a513a117
Kamil Jarosz <kjarosz19946@gmail.com>
2015-04-20 21:35:38 -0400

    Implement change command.

--- src/Core/LevelEditor/Control/Commands/ChangeEntityCommand.java
+++ src/Core/LevelEditor/Control/Commands/ChangeEntityCommand.java
@@ -8,6 +8,7 @@ import Core.LevelEditor.Models.DrawerSettings;
 import Core.LevelEditor.Models.EditorSettings;
 import Core.LevelEditor.Models.Entity;
 import Core.LevelEditor.Models.LevelModel;
+import Util.Size;
 
 public class ChangeEntityCommand extends LevelModelCommand {
 
@@ -23,7 +24,23 @@ public class ChangeEntityCommand extends LevelModelCommand {
    
    @Override
    public void perform(MouseEvent event) {
+      Point mouseCoords = snapToGrid(event.getPoint());
       
+      Rectangle bounds = getBounds(mStartPosition, mouseCoords);
+      mActiveEntity.setRect(bounds);
+   }
+   
+   private Rectangle getBounds(Point p1, Point p2) {
+      int xLeft = (p1.x < p2.x) ? p1.x : p2.x;
+      int xRight = (p1.x >= p2.x) ? p1.x : p2.x;
+      int yTop = (p1.y < p2.y) ? p1.y : p2.y;
+      int yBottom = (p1.y >= p2.y) ? p1.y : p2.y;
+      Size gridSize = mDrawer.getGridSize();
+      
+      return new Rectangle(
+            xLeft, yTop,
+            xRight - xLeft + gridSize.width,
+            yBottom - yTop + gridSize.height);
    }
    
    protected void setActiveEntity(Entity entity) {
