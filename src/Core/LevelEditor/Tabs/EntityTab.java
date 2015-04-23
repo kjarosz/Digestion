98f70ee918a28c75368af55e3df6e6f143d9a432
Kamil Jarosz <kjarosz19946@gmail.com>
2015-04-23 10:30:34 -0400

    Created Entity Tab

--- src/Core/LevelEditor/Tabs/EntityTab.java
+++ src/Core/LevelEditor/Tabs/EntityTab.java
@@ -1,7 +1,17 @@
 package Core.LevelEditor.Tabs;
 
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import Core.LevelEditor.Models.EntityModel;
 import Core.LevelEditor.Models.EntityModelList;
 
 public class EntityTab extends JPanel {
@@ -9,7 +19,58 @@ public class EntityTab extends JPanel {
    
    public EntityTab(EntityModelList entityModel) {
       mEntityModel = entityModel;
+      createWidgets();
    }
    
+   private void createWidgets() {
+      setLayout(new BorderLayout());
+      add(createList(), BorderLayout.CENTER);
+      add(createButtons(), BorderLayout.SOUTH);
+   }
    
+   private JScrollPane createList() {
+      JList<EntityModel> list = new JList<>(mEntityModel);
+      list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+      JScrollPane scroller = new JScrollPane(list);
+      return scroller;
+   }
+   
+   private JPanel createButtons() {
+      JPanel buttonPanel = new JPanel();
+      buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+      buttonPanel.setAlignmentX(CENTER_ALIGNMENT);
+      buttonPanel.add(createSaveButton());
+      buttonPanel.add(createLoadButton());
+      return buttonPanel;
+   }
+   
+   private JButton createSaveButton() {
+      JButton button = new JButton("Save");
+      button.addActionListener(createSaveCommand());
+      return button;
+   }
+   
+   private JButton createLoadButton() {
+      JButton button = new JButton("Load");
+      button.addActionListener(createLoadCommand());
+      return button;
+   }
+   
+   private ActionListener createSaveCommand() {
+      return new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            
+         }
+      };
+   }
+   
+   private ActionListener createLoadCommand() {
+      return new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            
+         }
+      };
+   }
 }
