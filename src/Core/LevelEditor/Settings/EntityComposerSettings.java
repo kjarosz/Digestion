59bc668d4f67a8ecaeaffc30f670f36060f749c6
Kamil Jarosz <kjarosz19946@gmail.com>
2015-04-26 09:50:01 -0400

    Put in component selection in settings.

--- src/Core/LevelEditor/Settings/EntityComposerSettings.java
+++ src/Core/LevelEditor/Settings/EntityComposerSettings.java
@@ -1,9 +1,19 @@
 package Core.LevelEditor.Settings;
 
 import Core.LevelEditor.Models.AbstractModel;
+import Core.LevelEditor.Models.ComponentModel;
 
 public class EntityComposerSettings extends AbstractModel {
-   public EntityComposerSettings() {
-      
+   private ComponentModel selectedModel;
+   
+   public ComponentModel getSelectedComponentModel() {
+      return selectedModel;
+   }
+   
+   public void setSelectedComponentModel(ComponentModel componentModel) {
+      if(!componentModel.equals(selectedModel)) {
+         firePropertyChangeEvent("component selected", selectedModel, componentModel);
+         selectedModel = componentModel;
+      }
    }
 }
