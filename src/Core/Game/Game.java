f0f9dc4ce028d4baa6cf49a912fd6d4899a74904
Kamil Jarosz <kjarosz19946@gmail.com>
2015-08-27 18:23:55 -0400

    Correct indentation.

--- src/Core/Game/Game.java
+++ src/Core/Game/Game.java
@@ -28,154 +28,154 @@ import Util.GameTimer;
 import Util.UnitConverter;
 
 public class Game {
-   private GameWindow mWindow;
-   
-   private MenuStack mMenuStack;
-   private GameCanvas mGameCanvas;
-	
-   private boolean mPaused;
-   private boolean mQuit;
+	private GameWindow mWindow;
 
-   private EntityFactory mEntityFactory;
-   private Level mLevel;
-   private World mBox2DWorld;
-   private EntityContainer mWorld;
+	private MenuStack mMenuStack;
+	private GameCanvas mGameCanvas;
+
+	private boolean mPaused;
+	private boolean mQuit;
+
+	private EntityFactory mEntityFactory;
+	private Level mLevel;
+	private World mBox2DWorld;
+	private EntityContainer mWorld;
 
 	public Game() {
 		mWindow = new GameWindow("Digestion");
 		mGameCanvas = new GameCanvas();
-      mEntityFactory = EntityFactory.getInstance();
+		mEntityFactory = EntityFactory.getInstance();
 		mPaused = true;
-      setupMenuStack();
+		setupMenuStack();
 	}
-   
-   private void setupMenuStack() {
-      mMenuStack = new MenuStack();
-      mWindow.switchTo(mMenuStack);
-      MainMenu mainMenu = new MainMenu(this, mMenuStack);
-      mainMenu.setBackground(Color.BLACK);
-      mMenuStack.pushScreen(mainMenu);
-   }
-	
-   public void startLevel(LevelLoadingScript loadingScript) {
-      if(!loadLevel(loadingScript)) {
-         mMenuStack.popScreen();
-      	return;
-      }
-      
-      MotionSystem.resetTimer();
-      
-      mQuit = false;
-      mMenuStack.popScreen(); // Get rid of loading screen.
-      mWindow.switchTo(mGameCanvas);
-      mWindow.update();
-      execute();
-   }
-   
-   private boolean loadLevel(LevelLoadingScript loadingScript) {
-      try {
-         mLevel = new Level();
-         loadingScript.loadLevel(mLevel);
-         mWorld = new EntityContainer();
-         mBox2DWorld = new World(mLevel.m_gravity);
-         loadingScript.createEntities(mBox2DWorld, mEntityFactory, mWorld);
-      } catch(PyException ex) {
-         ErrorLog logger = ErrorLog.getInstance();
-         logger.writeError(ex.toString());
-         return false;
-      }
-      return true;
-   }
-   
+
+	private void setupMenuStack() {
+		mMenuStack = new MenuStack();
+		mWindow.switchTo(mMenuStack);
+		MainMenu mainMenu = new MainMenu(this, mMenuStack);
+		mainMenu.setBackground(Color.BLACK);
+		mMenuStack.pushScreen(mainMenu);
+	}
+
+	public void startLevel(LevelLoadingScript loadingScript) {
+		if(!loadLevel(loadingScript)) {
+			mMenuStack.popScreen();
+			return;
+		}
+
+		MotionSystem.resetTimer();
+
+		mQuit = false;
+		mMenuStack.popScreen(); // Get rid of loading screen.
+		mWindow.switchTo(mGameCanvas);
+		mWindow.update();
+		execute();
+	}
+
+	private boolean loadLevel(LevelScript loadingScript) {
+		try {
+			mLevel = new Level();
+			loadingScript.loadLevel(mLevel);
+			mWorld = new EntityContainer();
+			mBox2DWorld = new World(mLevel.m_gravity);
+			loadingScript.createEntities(mBox2DWorld, mEntityFactory, mWorld);
+		} catch(PyException ex) {
+			ErrorLog logger = ErrorLog.getInstance();
+			logger.writeError(ex.toString());
+			return false;
+		}
+		return true;
+	}
+
 	public void pause() { 
-      mPaused = true;
-      PauseMenu pauseMenu = new PauseMenu(this, mMenuStack);
-      mMenuStack.pushScreen(pauseMenu);
-      mWindow.switchTo(mMenuStack);
-   }
-   
-   public void resume() {
-      mPaused = false;
-      mWorld.resetTimers();
-      MotionSystem.resetTimer();
-      mWindow.switchTo(mGameCanvas);
-   }
-   
-   public void quitToMenu() {
-      mQuit = true;
-   }
-   
+		mPaused = true;
+		PauseMenu pauseMenu = new PauseMenu(this, mMenuStack);
+		mMenuStack.pushScreen(pauseMenu);
+		mWindow.switchTo(mMenuStack);
+	}
+
+	public void resume() {
+		mPaused = false;
+		mWorld.resetTimers();
+		MotionSystem.resetTimer();
+		mWindow.switchTo(mGameCanvas);
+	}
+
+	public void quitToMenu() {
+		mQuit = true;
+	}
+
 	private void execute() {
-      mPaused = false;
-		
-      setFocusObject();
-      
-      GameTimer fpsUpdateTimer = new GameTimer();
-      fpsUpdateTimer.setTimeInterval(5000);
-      
-      int frameCounter = 0;
-      boolean escProcessed = false;
+		mPaused = false;
+
+		setFocusObject();
+
+		GameTimer fpsUpdateTimer = new GameTimer();
+		fpsUpdateTimer.setTimeInterval(5000);
+
+		int frameCounter = 0;
+		boolean escProcessed = false;
 		while(!mQuit) {
 			if(mPaused) {
 				if(KeyManager.isKeyPressed(KeyEvent.VK_ESCAPE) && !escProcessed) {
-               escProcessed = true;
-               resume();
-            }
+					escProcessed = true;
+					resume();
+				}
 			} else {
-            ControlSystem.manipulate(mWorld);
-            MotionSystem.move(mBox2DWorld, mWorld);
-            AnimationSystem.animate(mWorld);
-            DrawingSystem.draw(mWorld, mGameCanvas);
-            
-            mWindow.update();
-            
-            if(KeyManager.isKeyPressed(KeyEvent.VK_ESCAPE) && !escProcessed) {
-               escProcessed = true;
-               pause();
-            }
+				ControlSystem.manipulate(mWorld);
+				MotionSystem.move(mBox2DWorld, mWorld);
+				AnimationSystem.animate(mWorld);
+				DrawingSystem.draw(mWorld, mGameCanvas);
+
+				mWindow.update();
+
+				if(KeyManager.isKeyPressed(KeyEvent.VK_ESCAPE) && !escProcessed) {
+					escProcessed = true;
+					pause();
+				}
 			}
-         
-         if(!KeyManager.isKeyPressed(KeyEvent.VK_ESCAPE) && escProcessed)
-            escProcessed = false;
-         
-         frameCounter++;
-         if(fpsUpdateTimer.hasTimeIntervalPassed()) {
-            updateFPS(frameCounter, fpsUpdateTimer.getElapsedTime());
-            fpsUpdateTimer.reset();
-            frameCounter = 0;
-         }
+
+			if(!KeyManager.isKeyPressed(KeyEvent.VK_ESCAPE) && escProcessed)
+				escProcessed = false;
+
+			frameCounter++;
+			if(fpsUpdateTimer.hasTimeIntervalPassed()) {
+				updateFPS(frameCounter, fpsUpdateTimer.getElapsedTime());
+				fpsUpdateTimer.reset();
+				frameCounter = 0;
+			}
 		}
 		performCleanUp();
-		
+
 		mWindow.setTitle("Digestion");
-      
-      mWindow.switchTo(mMenuStack);
+
+		mWindow.switchTo(mMenuStack);
 	}
-	
+
 	private void performCleanUp() {
 		mLevel = null;
 		mWorld = null;
 		mBox2DWorld = null;
 	}
-	
+
 	private void setFocusObject() {
-	   for(int i = 0; i < EntityContainer.MAXIMUM_ENTITIES; i++) {
-	      int entityMask = mWorld.getEntityMask(i);
-	      if((entityMask & EntityContainer.ENTITY_FOCUSABLE) != 0) {
-	         EntityComponents components = mWorld.accessComponents(i);
-	         
-	         Vec2 px_levelSize = UnitConverter.metersToPixels(mLevel.m_size);
-	         GameViewport viewport = new GameViewport(px_levelSize, new Vec2(mWindow.getWidth(), mWindow.getHeight()));
-	         viewport.setFocusObject(components);
-	         
-	         mGameCanvas.setViewport(viewport);
-	         break;
-	      }
-	   }
+		for(int i = 0; i < EntityContainer.MAXIMUM_ENTITIES; i++) {
+			int entityMask = mWorld.getEntityMask(i);
+			if((entityMask & EntityContainer.ENTITY_FOCUSABLE) != 0) {
+				EntityComponents components = mWorld.accessComponents(i);
+
+				Vec2 px_levelSize = UnitConverter.metersToPixels(mLevel.m_size);
+				GameViewport viewport = new GameViewport(px_levelSize, new Vec2(mWindow.getWidth(), mWindow.getHeight()));
+				viewport.setFocusObject(components);
+
+				mGameCanvas.setViewport(viewport);
+				break;
+			}
+		}
 	}
-	
+
 	private void updateFPS(int frameCount, long timePassed) {
-	   double fps = (double)frameCount / (timePassed/5000.0);
-	   mWindow.setTitle("Digestion - " + fps + " FPS");
+		double fps = (double)frameCount / (timePassed/5000.0);
+		mWindow.setTitle("Digestion - " + fps + " FPS");
 	}
 }
