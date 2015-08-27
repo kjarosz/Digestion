95e64af56b66070fae6841fab33dd1fa0f0bfe88
Kamil Jarosz <kjarosz19946@gmail.com>
2015-08-27 18:23:31 -0400

    Correct indentation.

--- src/Level/Level.java
+++ src/Level/Level.java
@@ -5,17 +5,17 @@ import java.awt.image.BufferedImage;
 import org.jbox2d.common.Vec2;
 
 public class Level {
-   public String name;
+	public String name;
 	public Vec2 m_size;
 	public Vec2 m_gravity;
-   
-   public BufferedImage mBackground;
+
+	public BufferedImage mBackground;
 
 	public Level() {
-      name = "";
-      m_size = new Vec2(25.0f, 18.75f);
-      m_gravity = new Vec2(0.0f, 9.81f);
+		name = "";
+		m_size = new Vec2(25.0f, 18.75f);
+		m_gravity = new Vec2(0.0f, 9.81f);
 	}
-	
-	
+
+
 }
