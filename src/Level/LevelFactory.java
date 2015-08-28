cac1321615d1ab493297c7974912b0a2f6f55e16
Kamil Jarosz <kjarosz19946@gmail.com>
2015-08-28 17:54:02 -0400

    Add logic to look for level scripts inside folders.

--- src/Level/LevelFactory.java
+++ src/Level/LevelFactory.java
@@ -1,13 +1,35 @@
 package Level;
 
+import java.io.File;
+import java.util.Arrays;
+
 
 public abstract class LevelFactory {
+   private static final String LEVEL_DIRECTORY = "resources/Levels/";
+   private static final String LEVEL_SCRIPTNAME = "Level.json";
 
-	public static String[] getLevelScripts() {
-
-	}
-
-	public Level loadLevel(String levelScriptName) {
-
-	}
+   private static boolean hasLevelScript(File directory) {
+      for(File file: directory.listFiles()) {
+         if(file.getName().equals(LEVEL_SCRIPTNAME)) {
+            return true;
+         }
+      }
+      return false;
+   }
+   
+   public static String[] getLevelScripts() {
+      File directory = new File(LEVEL_DIRECTORY);
+      return (String[])Arrays
+           .stream(directory.listFiles())
+           .filter((file) -> file.isDirectory() && hasLevelScript(file))
+           .map((levelDir) -> levelDir.getName())
+           .toArray();
+   }
+   
+   public static Level loadLevel(String scriptname) {
+      File levelScript = new File(LEVEL_DIRECTORY  
+           + File.separator + scriptname 
+           + File.separator + LEVEL_SCRIPTNAME);
+      
+   }
 }
