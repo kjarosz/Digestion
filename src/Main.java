38a0383417f142e1faa908e366a2ee3d75c025c8
Kamil Jarosz <kjarosz19946@gmail.com>
2015-08-29 03:22:01 -0400

    Reduce flicker on resize.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Core.Game.Game;
 
 public class Main {
 	public static void main(String arguments[]) {
+	   System.setProperty("sun.awt.noerasebackground", "true");
 		new Game().start();
-		
 	}
 }
