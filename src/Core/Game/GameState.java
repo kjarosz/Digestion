302d33d7fd78f27077ea6c9a0662a251753b64de
Kamil Jarosz <kjarosz19946@gmail.com>
2015-08-28 22:42:53 -0400

    Add screen size as parameter when switching to a new state.

--- src/Core/Game/GameState.java
+++ src/Core/Game/GameState.java
@@ -1,10 +1,12 @@
 package Core.Game;
 
+import java.awt.Dimension;
+
 import Graphics.CanvasInterface;
 
 public interface GameState {
    /* Called when state machine is switching to this state. */
-   public void beforeSwitch();
+   public void beforeSwitch(Dimension screenSize);
    
    /* All mouse/keyboard/other events happen here. */
    public void handleEvents();
