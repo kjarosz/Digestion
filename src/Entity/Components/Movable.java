5f4aa26953ccf3f72f7b674ae9b3efc714d44d43
Kamil Jarosz <kjarosz19946@gmail.com>
2015-08-30 19:31:22 -0400

    Add velocity, force, and mass tracking to Movable component.

--- src/Entity/Components/Movable.java
+++ src/Entity/Components/Movable.java
@@ -2,12 +2,14 @@ package Entity.Components;
 
 import Util.Vector2D;
 
-
 public class Movable {
-	
-	public Vector2D actingForces;
+	public Vector2D velocity;
+	public Vector2D netForce;
+	public double mass;
    
    public Movable() {
-   	actingForces = new Vector2D(0.0f, 0.0f);
+      velocity = new Vector2D(0.0, 0.0);
+      netForce = new Vector2D(0.0, 0.0);
+      mass = 1.0;
    }
 }
