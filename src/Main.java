848e3d9ec3106a91db494127647a4944b595bd24
Kamil Jarosz <kjarosz19946@gmail.com>
2015-09-02 21:13:12 -0400

    Remove openql trigger.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import Core.Game.Game;
 
 public class Main {
    public static void main(String arguments[]) {
-      System.setProperty("sun.java2d.opengl", "true");
       SwingUtilities.invokeLater(new Runnable() {
          @Override
          public void run() {
