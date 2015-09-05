cd8fecdbeb39a1bb93cc60c1a9d124bdd125514f
Kamil Jarosz <kjarosz19946@gmail.com>
2015-09-05 09:23:59 -0400

    Convert Runnable function to labmda expression.

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,10 @@ import Core.Game.Game;
 
 public class Main {
    public static void main(String arguments[]) {
-      SwingUtilities.invokeLater(new Runnable() {
-         @Override
-         public void run() {
-            new Game().start();
-         }
+      SwingUtilities.invokeLater(() -> {
+          new Game().start();
       });
    }
+   
+
 }
