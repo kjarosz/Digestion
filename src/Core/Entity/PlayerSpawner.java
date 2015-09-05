299e2e049989cb7669d1ff949ea6e2305f2c70bb
Kamil Jarosz <kjarosz19946@gmail.com>
2015-09-05 15:14:15 -0400

    Set Player size correctly.

--- src/Core/Entity/PlayerSpawner.java
+++ src/Core/Entity/PlayerSpawner.java
@@ -6,7 +6,6 @@ import java.io.File;
 import Entity.EntityComponents;
 import Entity.EntitySpawner;
 import Entity.Components.Controllable;
-import Entity.Components.Drawable;
 import Input.ControlFunction;
 import Level.EntityContainer;
 import Util.Vector2D;
@@ -14,32 +13,44 @@ import Util.Vector2D;
 public class PlayerSpawner extends EntitySpawner {
    private final static String ENTITY_IMAGE = "Players" + File.separator + "Gordon" + File.separator + "standing.gif";
    
-   private final Vector2D PLAYER_SIZE = new Vector2D(32, 64);
-
    private final Vector2D LEFT_FORCE = new Vector2D(-500, 0);
    private final Vector2D RIGHT_FORCE = new Vector2D(500, 0);
    private final Vector2D UP_FORCE = new Vector2D(0, -500);
    
+   private EntityComponents components;
+
    @Override
    public int spawn(Vector2D position, Vector2D size, EntityComponents components) {
+      this.components = components;
+
       int mask = EntityContainer.ENTITY_NONE;
-      mask |= makeMovable(components, PLAYER_SIZE, size);
-      mask |= makeControllable(components.controllable);
-      mask |= makeDrawable(components.drawable);
+      mask |= makeDrawable();
+      mask |= makeMovable(position);
+      mask |= makeControllable();
       mask |= EntityContainer.ENTITY_FOCUSABLE;
       return   mask;
    }
+
+   private int makeDrawable() {
+      components.drawable.image = loadImage(ENTITY_IMAGE);
+      return EntityContainer.ENTITY_DRAWABLE;
+   }
    
-   private int makeMovable(EntityComponents components, Vector2D position, Vector2D size) {
-      makeAABB(components, position, size);
+   private int makeMovable(Vector2D position) {
+      makeAABB(components, position, getImageDimensions());
       components.movable.terminalVelocity = 500;
       return EntityContainer.ENTITY_COLLIDABLE | EntityContainer.ENTITY_MOVABLE;
    }
+
+   private Vector2D getImageDimensions() {
+      return new Vector2D(components.drawable.image.getWidth(), 
+                          components.drawable.image.getHeight());
+   }
    
-   private int makeControllable(Controllable controllable) {
-      constructMovingKeyMapping(controllable, KeyEvent.VK_A, LEFT_FORCE);
-      constructJumpKeyMapping(controllable, KeyEvent.VK_W, UP_FORCE);
-      constructMovingKeyMapping(controllable, KeyEvent.VK_D, RIGHT_FORCE);
+   private int makeControllable() {
+      constructMovingKeyMapping(components.controllable, KeyEvent.VK_A, LEFT_FORCE);
+      constructJumpKeyMapping(components.controllable, KeyEvent.VK_W, UP_FORCE);
+      constructMovingKeyMapping(components.controllable, KeyEvent.VK_D, RIGHT_FORCE);
       
       return EntityContainer.ENTITY_CONTROLLABLE;
    }
@@ -76,10 +87,5 @@ public class PlayerSpawner extends EntitySpawner {
    		@Override
    		public void keyReleased(EntityComponents components) {}
    	});
-   }
-   
-   private int makeDrawable(Drawable drawable) {
-      drawable.image = loadImage(ENTITY_IMAGE);
-      return EntityContainer.ENTITY_DRAWABLE;
    }
 }
